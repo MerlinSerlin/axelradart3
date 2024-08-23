60f851466a2c0a15473e8a0bfaf3a6ed771c2866
Marshall Serlin <marshallserlin@gmail.com>
2024-08-22 19:24:55 -0700

    quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import MainMenu from "@/components/main-menu/main-menu";
 import CardWithImage from "@/components/ui/card-with-image";
-import { Main } from "next/document";
 
 
 
