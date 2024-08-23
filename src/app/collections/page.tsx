7e06173b299a0b46f002f3e78c34ae81fb178445
Marshall Serlin <marshallserlin@gmail.com>
2024-08-22 22:20:10 -0700

    prefetch false for next link

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
           />
           <CardFooter className="pt-6 w-11/12 rounded-lg border bg-card text-card-foreground shadow-sm">
             <Button className="w-full">
-              <Link href={`/collections/${image.theme}`}>
+              <Link prefetch={false} href={`/collections/${image.theme}`}>
                 View Collection
               </Link>
             </Button>
